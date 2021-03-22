2e59e776b92da097a1d22f428344a678729e477d
Morphi007 <78938079+Morphi007@users.noreply.github.com>
2021-03-22 03:33:11 -0400

    Add files via upload

---
+++ ejercicio_1 practica 6.cs
@@ -0,0 +1,144 @@
+﻿using System;
+
+namespace practica_06_ejercicio_1
+{
+
+    class persona{
+
+//--------------cedula-----------------------
+        private int cedula;
+        public int Cedula
+        {
+            set
+            {
+                cedula = value;
+            }
+            get
+            {return cedula;}
+
+              }
+    //------------Nombre------------------------//*
+        private string nombre;
+        public string Nombre
+          {
+        set
+        {
+                nombre = value;
+        }
+
+            get
+            {return nombre;}
+
+
+        }
+
+//----------------apellido------------------------//*
+
+        private string apellido;
+        public string Apellido
+        {
+            set
+            {
+                apellido = value;
+
+            }
+            get { return apellido; }
+
+
+        }
+
+        //-------------------edad------------------------
+
+
+        private int edad;
+        public int Edad
+        {
+
+            set 
+            {
+                edad = value;
+            }
+            get { return edad; }
+        }
+    
+    
+      public void imprimir()
+        {
+
+            Console.WriteLine("Nombre: "+Nombre);
+            Console.WriteLine("Apellido: " + Apellido);
+            Console.WriteLine("Edad: " + Edad);
+            Console.WriteLine("Cedula: " + Cedula);
+
+        }
+    
+    
+           
+    
+    
+    }
+
+
+
+    class Profesor:persona
+    {
+
+        private float sueldo;
+        public float Sueldo
+        {
+            set
+            {
+                sueldo =value;
+            }
+            get { return sueldo; }
+            
+
+        }
+
+        new public void imprimir()
+        
+        {
+
+           base.imprimir();
+
+            Console.WriteLine("sueldo "+Sueldo);
+
+
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            persona  persona1 = new persona();
+           
+            persona1.Cedula =4025482;
+            persona1.Nombre = "Morphi";
+            persona1.Apellido = "MARTINEZ";
+            persona1.Edad = 20;
+            persona1.imprimir();
+            Console.WriteLine("\n");
+
+            Profesor profe = new Profesor();
+            profe.Cedula =0154796;
+            profe.Nombre = "profesor";
+            profe.Apellido = "moreta";
+            profe.Edad = 25;
+            profe.Sueldo =45000;
+            profe.imprimir();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ejercicio_2 practica 6.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace ejercicio_2
+{
+    
+    
+    class Contacto
+    {
+
+        private string nombre;
+        public string Nombre
+        {
+
+            set
+            {
+                nombre = value;
+            }
+            get { return nombre; }
+        }
+//------------------------------------------///
+        private string apellido;
+        public string Apeliddo
+        {
+
+            set { nombre = value; }
+            get { return  nombre;}
+        }
+        //---------------------------------------------///
+
+        private string telefono;
+        public string Telefono
+        {
+
+            set { telefono = value; }
+            get { return telefono; }
+        }
+        //--------------------------------------------------///
+
+
+        private string dirrecion;
+        public string Dirrecion
+        {
+
+            set { dirrecion = value; }
+            get { return dirrecion; }
+        }
+
+        //--------------------------------------------------------///
+
+
+        public void SetContacto()
+        {
+            Console.WriteLine("ingresa tu Nombre\n");
+            nombre = Console.ReadLine();
+            Console.WriteLine("ingresa tu apellido\n");
+            apellido = Console.ReadLine();
+            Console.WriteLine("ingresa tu telefono\n");
+            telefono = Console.ReadLine();
+            Console.WriteLine("ingresa tu dirrecion\n");
+            dirrecion = Console.ReadLine();
+        }
+
+
+
+        public void saludar()
+        {
+
+            Console.WriteLine("Hola soy:" +nombre, "Apellido"+apellido," telefono"+telefono," dirrecion"+dirrecion);
+        }
+
+
+
+    
+    
+
+
+    class ProbarContacto:Contacto
+    {
+        static void Main(string[] args)
+        {
+        
+
+                Contacto contacto1 = new Contacto();
+                Contacto contacto2 = new Contacto();
+                Console.WriteLine("contacto #1 \n");
+                contacto1.SetContacto();
+                contacto1.saludar();
+                Console.WriteLine("------------------");
+                Console.WriteLine("contacto #2 \n");
+                contacto1.SetContacto();
+                contacto1.saludar();
+
+
+
+
+            }
+        }
+    }
+}
